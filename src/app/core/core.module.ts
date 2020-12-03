9e5426b2d45fd4521d3f8519c897e11b98f8dbf2
Felipe Honório <felipe.h.camargo@gmail.com>
2020-12-03 19:12:49 -0300

    criaçao core module

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CoreRoutingModule } from './core-routing.module';
+import { CorePage } from './page/core.page';
+import { BrowserModule } from '@angular/platform-browser';
+
+
+@NgModule({
+  declarations: [CorePage],
+  imports: [
+    BrowserModule,
+    CoreRoutingModule
+  ],
+  bootstrap: [CorePage]
+})
+export class CoreModule { }
